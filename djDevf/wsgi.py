a81f772b88c93b3cc643a9475a88a11ca043f2e8
Inaki Cabo <icaboalo@gmail.com>
2016-03-11 09:45:46 -0600

    modified default settings

--- djDevf/wsgi.py
+++ djDevf/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# for local use
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djDevf.settings.local")
+
+#for default use
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djDevf.settings")
+
 
 application = get_wsgi_application()
