d8dff25feb3482392308c79b98b319a98801a7da
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-03-13 13:15:20 -0600

    choices

--- user/models.py
+++ user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
+from utils.choices import *
 
 # Create your models here.
 class MyUser(models.Model):
@@ -18,6 +19,8 @@ class MyUser(models.Model):
     is_validated = models.BooleanField(blank=True, default=False)
     job = models.CharField(max_length=50, blank=True)
     description = models.TextField(blank=True, max_length=140)
+    batch = models.CharField(blank=True, choices=BATCH_CHOICES, max_length=10)
+    skills = models.CharField(max_length=15, blank=True, choices=SKILL_CHOICES)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{10,15}$', message="Phone number must be entered in the format: '55-5555-5555'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=13) # validators should be a list
 
