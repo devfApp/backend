b9eb709efc2ebdf71e562d656037fdd64ab1795a
Inaki Cabo <icaboalo@gmail.com>
2016-03-13 11:41:05 -0600

    media for profile pic

--- djDevf/settings/local.py
+++ djDevf/settings/local.py
@@ -18,3 +18,5 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- user/models.py
+++ user/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class MyUser(models.Model):
 
     class Meta:
         verbose_name = "MyUser"
-        verbose_name_plural = "MyUsers"
+        verbose_name_plural = "MyUser"
 
     #Relations
+    user = models.OneToOneField(User, related_name='user')
 
     #Attributes
+    date_added = models.DateTimeField(auto_now_add=True)
+    profile_pic = models.ImageField(upload_to='profile_pic/', blank=True)
+    is_validated = models.BooleanField(blank=True, default=False)
+    description = models.TextField(blank=True, max_length=140)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{10,15}$', message="Phone number must be entered in the format: '55-5555-5555'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=13) # validators should be a list
 
     def __str__(self):
-        pass
+    	return (self.user.username)
     
