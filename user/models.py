316bbf88f5172039246d919016db47465e2e67e4
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-03-13 13:37:14 -0600

    many skills options

--- user/models.py
+++ user/models.py
@@ -4,6 +4,19 @@ from django.core.validators import RegexValidator
 from utils.choices import *
 
 # Create your models here.
+class Skill(models.Model):
+
+    class Meta:
+        verbose_name = "Skill"
+        verbose_name_plural = "Skills"
+
+    #Attributes
+    skill = models.CharField(max_length=20)
+
+    def __str__(self):
+    	return self.skill
+    
+    
 class MyUser(models.Model):
 
     class Meta:
@@ -20,10 +33,10 @@ class MyUser(models.Model):
     job = models.CharField(max_length=50, blank=True)
     description = models.TextField(blank=True, max_length=140)
     batch = models.CharField(blank=True, choices=BATCH_CHOICES, max_length=10)
-    skills = models.CharField(max_length=15, blank=True, choices=SKILL_CHOICES)
+    skills = models.ManyToManyField(Skill)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{10,15}$', message="Phone number must be entered in the format: '55-5555-5555'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=13) # validators should be a list
 
     def __str__(self):
     	return (self.user.username)
-    
+
