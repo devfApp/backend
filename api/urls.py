db8a110eadaefad9eb6f4f70ae3cb5fe2053bd6a
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-03-15 08:41:58 -0600

    comments for swagger docs

--- api/urls.py
+++ api/urls.py
@@ -4,18 +4,19 @@ from rest_framework.authtoken import views as token_view
 
 urlpatterns = [
 	#Events urls
-	url(r'event/$', api_view.EventView.as_view()),
+	url(r'event/$', api_view.EventView.as_view(), name='event_api'),
 	url(r'event/(?P<pk>[0-9]+)/$', api_view.EventDetailView.as_view(), name='event_api_detail'),
 
 	#Batch urls
-	url(r'batch/$', api_view.BatchView.as_view()),
+	url(r'batch/$', api_view.BatchView.as_view(), name='batch_api'),
+	url(r'batch/(?P<pk>[0-9]+)/$', api_view.BatchDetailView.as_view(), name='batch_api_detail'),
 
 	#Skill urls
 	url(r'skill/$', api_view.SkillView.as_view(), name='skill_api'),
 	url(r'skill/(?P<pk>[0-9]+)/$', api_view.SkillDetailView.as_view(), name='skill_api_detail'),
 
 	#MyUser urls
-	url(r'user/$', api_view.MyUserView.as_view()),
+	url(r'user/$', api_view.MyUserView.as_view(), name='user_api'),
 	url(r'user/(?P<pk>[0-9]+)/$', api_view.MyUserDetailView.as_view(), name='user_api_detail'),
 
 	#DOCS
--- api/views.py
+++ api/views.py
@@ -10,32 +10,68 @@ from .serializers import *
 # Create your views here.
 #Event Views
 class EventView(generics.ListCreateAPIView):
+	"""
+	EVENT object list and create object
+	"""
+
 	queryset = Event.objects.all()
 	serializer_class = EventSerializer
 
 class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
+	"""
+	EVENT object view and edit
+	"""
+
 	queryset = Event.objects.all()
 	serializer_class = EventSerializer
 
 #Batch Views
 class BatchView(generics.ListCreateAPIView):
+	"""
+	BATCH object list and create object
+	"""
+
+	queryset=Batch.objects.all()
+	serializer_class=BatchSerializer
+
+class BatchDetailView(generics.RetrieveUpdateDestroyAPIView):
+	"""
+	BATCH object view and edit
+	"""
+
 	queryset=Batch.objects.all()
 	serializer_class=BatchSerializer
 
 #Skill Views
 class SkillView(generics.ListCreateAPIView):
+	"""
+	SKILL object list and create object
+	"""
+
 	queryset=Skill.objects.all()
 	serializer_class=SkillSerializer
 
 class SkillDetailView(generics.RetrieveUpdateDestroyAPIView):
+	"""
+	SKILL object view and edit
+	"""
+
 	queryset=Skill.objects.all()
 	serializer_class=SkillSerializer
 
 #MyUse Views
 class MyUserView(generics.ListCreateAPIView):
+	"""
+	MYUSER object list and create object
+	"""
+
 	queryset = MyUser.objects.all()
 	serializer_class = MyUserSerializer
 
 class MyUserDetailView(generics.RetrieveUpdateDestroyAPIView):
+	"""
+	MYUSER object view and edit
+	"""
+
 	queryset = MyUser.objects.all()
 	serializer_class = MyUserSerializer
