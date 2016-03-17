b24e1fb4bfd3a5807c223e9dadd519e7dcfb4449
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-03-16 18:16:17 -0600

    prep for filters

--- djDevf/settings/base.py
+++ djDevf/settings/base.py
@@ -79,6 +79,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djDevf.wsgi.application'
 
+REST_FRAMEWORK = {
+   # 'DEFAULT_AUTHENTICATION_CLASSES': (
+       # 'rest_framework.authentication.BasicAuthentication',
+   #     'rest_framework.authentication.SessionAuthentication',
+   #     'rest_framework.authentication.TokenAuthentication',
+   # ),
+   # 'DEFAULT_PERMISSION_CLASSES': (
+   #     'rest_framework.permissions.IsAuthenticated',
+   #     # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+   # ),
+   'DEFAULT_FILTER_BACKENDS': (
+       'rest_framework.filters.DjangoFilterBackend',
+   ),
+   'SEARCH_PARAM': 'q',
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
