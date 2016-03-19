d3b5c3e25629eb37796a58e71af14294a675555f
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-03-19 11:27:41 -0600

    deployment.py

--- djDevf/wsgi.py
+++ djDevf/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djDevf.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djDevf.settings.deployment")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
