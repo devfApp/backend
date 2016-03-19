bf78211f95c23f70ba19a3a55fa28f72bb8d3715
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-03-19 11:26:20 -0600

    deployment.py

---
+++ djDevf/settings/deployment.py
@@ -0,0 +1,18 @@
+from .base import 
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd2mjs5kgl18go2',
+        'USER': 'uteqffkoqsvrlh',
+        'PASSWORD': 'gFSY0nnebfpkSBtEksbvC9SwTc',
+        'HOST': 'ec2-54-83-22-48.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- djDevf/settings/local.py
+++ djDevf/settings/local.py
@@ -6,23 +6,23 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd2mjs5kgl18go2',
-        'USER': 'uteqffkoqsvrlh',
-        'PASSWORD': 'gFSY0nnebfpkSBtEksbvC9SwTc',
-        'HOST': 'ec2-54-83-22-48.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'd2mjs5kgl18go2',
+#         'USER': 'uteqffkoqsvrlh',
+#         'PASSWORD': 'gFSY0nnebfpkSBtEksbvC9SwTc',
+#         'HOST': 'ec2-54-83-22-48.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Static files (CSS, JavaScript, Images)
