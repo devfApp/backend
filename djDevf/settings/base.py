4ee26a43e778bdf7b11d47f5c806175d83f6a89a
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-03-28 18:23:45 -0600

    keys

--- djDevf/settings/base.py
+++ djDevf/settings/base.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-from .keys import *
+# from .keys import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = djangoKey
 
-SECRET_KEY = djangoKey
+# SECRET_KEY = djangoKey
 
 ALLOWED_HOSTS = []
 
