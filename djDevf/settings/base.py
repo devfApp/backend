80b866961de731a9402066e8c8633fe7264cc719
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-03-28 18:30:11 -0600

    revert keys

--- djDevf/settings/base.py
+++ djDevf/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = djangoKey
 
-SECRET_KEY = ''
+SECRET_KEY = '(#o@k!5a1$)*jrfevryxict9#di2ujhyal9v3w+xc5=4hg$k)$'
 
 ALLOWED_HOSTS = []
 
