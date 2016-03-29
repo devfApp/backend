fceb8cdeb70105514f61784af4bfb6be976e467a
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-03-28 19:03:43 -0600

    return token

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,10 @@ from . import views as api_view
 from rest_framework.authtoken import views as token_view
 
 urlpatterns = [
+
+	#Token url
+	url(r'api-token-auth/', token_view.obtain_auth_token),
+
 	#Events urls
 	url(r'event/$', api_view.EventView.as_view(), name='event_api'),
 	url(r'event/(?P<pk>[0-9]+)/$', api_view.EventDetailView.as_view(), name='event_api_detail'),
--- djDevf/settings/base.py
+++ djDevf/settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     #API
     'rest_framework',
     'rest_framework_swagger',
+    'rest_framework.authtoken',
 
 ]
 
@@ -82,15 +83,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'djDevf.wsgi.application'
 
 REST_FRAMEWORK = {
-   # 'DEFAULT_AUTHENTICATION_CLASSES': (
-       # 'rest_framework.authentication.BasicAuthentication',
-   #     'rest_framework.authentication.SessionAuthentication',
-   #     'rest_framework.authentication.TokenAuthentication',
-   # ),
-   # 'DEFAULT_PERMISSION_CLASSES': (
-   #     'rest_framework.permissions.IsAuthenticated',
-   #     # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-   # ),
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.BasicAuthentication',
+       'rest_framework.authentication.SessionAuthentication',
+       'rest_framework.authentication.TokenAuthentication',
+   ),
+   'DEFAULT_PERMISSION_CLASSES': (
+       'rest_framework.permissions.IsAuthenticated',
+       'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+   ),
    'DEFAULT_FILTER_BACKENDS': (
        'rest_framework.filters.DjangoFilterBackend',
    ),
