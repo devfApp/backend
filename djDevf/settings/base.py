8a9cebb6a4149fdf6d03b77f1127fcea9921b538
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-03-28 18:27:02 -0600

    keys

--- djDevf/settings/base.py
+++ djDevf/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = djangoKey
 
-# SECRET_KEY = djangoKey
+SECRET_KEY = ''
 
 ALLOWED_HOSTS = []
 
