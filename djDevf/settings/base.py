012bd588ff2438dbdb83527d6309c1dfea332c47
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-04-08 17:57:03 -0500

    deleted token

--- djDevf/settings/base.py
+++ djDevf/settings/base.py
@@ -86,12 +86,12 @@ REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': (
        'rest_framework.authentication.BasicAuthentication',
        'rest_framework.authentication.SessionAuthentication',
-       'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+       # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
    ),
-   'DEFAULT_PERMISSION_CLASSES': (
-       'rest_framework.permissions.IsAuthenticated',
-       'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-   ),
+   # 'DEFAULT_PERMISSION_CLASSES': (
+   #     'rest_framework.permissions.IsAuthenticated',
+   #     'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+   # ),
    'DEFAULT_FILTER_BACKENDS': (
        'rest_framework.filters.DjangoFilterBackend',
    ),
