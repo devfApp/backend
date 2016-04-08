0f220890809cd0a0373d4d277d5da07c05b02a1b
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-04-07 20:34:00 -0500

    test

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
 	#Token url
 	# url(r'api-token-auth/', token_view.obtain_auth_token),
-	url(r'api-token-auth/', jwt_views.obtain_jwt_token),
+	url(r'api-token-auth', jwt_views.obtain_jwt_token),
 
 	#Events urls
 	url(r'event/$', api_view.EventView.as_view(), name='event_api'),
