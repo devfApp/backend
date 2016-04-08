fb6944fc30d0e9c2920fca919cbd2d2af2051155
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-04-07 20:36:46 -0500

    test

--- djDevf/settings/base.py
+++ djDevf/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get("S_KEY")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
